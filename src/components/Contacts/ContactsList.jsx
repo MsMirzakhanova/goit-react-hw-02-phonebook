3393aa3aa6aff2a6c90f07d63ea60f9c43369464
MsMirzakhanova <98583249+MsMirzakhanova@users.noreply.github.com>
2022-12-18 17:29:41 +0200

    without props

--- src/components/Contacts/ContactsList.jsx
+++ src/components/Contacts/ContactsList.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 
 
@@ -16,15 +16,15 @@ export const ContactsList = ({ contacts, onDeleteContact}) => (
 )
 
 
-ContactList.propTypes = {
-  contacts: PropTypes.arrayOf(
-    PropTypes.exact({
-      id: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      number: PropTypes.string.isRequired,
-    })
-  ).isRequired,
-  onContactRemove: PropTypes.func.isRequired,
-};
+// ContactList.propTypes = {
+//   contacts: PropTypes.arrayOf(
+//     PropTypes.exact({
+//       id: PropTypes.string.isRequired,
+//       name: PropTypes.string.isRequired,
+//       number: PropTypes.string.isRequired,
+//     })
+//   ).isRequired,
+//   onContactRemove: PropTypes.func.isRequired,
+// };
 
     
